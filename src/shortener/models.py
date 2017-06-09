5b959749ad41ccb05bdc6e6d65d89d8f56cb4053
martynenko-e <martynenko.evhen@gmail.com>
2017-06-09 20:03:29 +0300

    Add first models

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,6 +1,43 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from utils import code_generator, create_shortcode
 
 # Create your models here.
+
+
+class ShortUrlManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(ShortUrlManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = ShortUrl.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i=new_codes)
+
+
+class ShortUrl(models.Model):
+    url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=20, unique=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    objects = ShortUrlManager()
+
+    def save(self, *args, **kwargs):
+        if not self.shortcode:
+            self.shortcode = create_shortcode(self)
+        super(ShortUrl, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.url)
+
+    def __unicode__(self):
+        return str(self.url)
